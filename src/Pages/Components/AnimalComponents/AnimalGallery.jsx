dc40848e713acd95b936be5f77f8689b7df7b24d
ElioFezollari <fezollarielio@gmail.com>
2024-06-03 13:50:35 -0400

    Changed search box design

--- src/Pages/Components/AnimalComponents/AnimalGallery.jsx
+++ src/Pages/Components/AnimalComponents/AnimalGallery.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAll } from "../../Services/getAll";
 import AnimalCard from "./AnimalCard";
+import magnifying from '../../../assets/magnifying.svg'
 const AnimalGallery = ({ animal,pictures }) => {
   const [animals, setAnimals] = useState([]);
   const [filteredSearch, setFilteredSearch] = useState("");
@@ -14,13 +15,16 @@ const AnimalGallery = ({ animal,pictures }) => {
   return (
     <div className="animal-gallery">
       <div className="animal-gallery-wrapper">
+        <div className="animal-search-wrapper">
         <input
           value={filteredSearch}
           onChange={(e) => setFilteredSearch(e.target.value)}
           className="animal-search"
           type="text"
-          placeholder={` \u{1F50E} Search for ${animal}`}
+          placeholder={` Search for ${animal}`}
         />
+        <img src={magnifying} alt="" />
+      </div>
       </div>
       <div className="animal-card-wrapper">
         <AnimalCard filteredAnimals={filteredAnimals} animal={animal} pictures={pictures}/>
--- src/Pages/Components/AnimalComponents/AnimalHero.jsx
+++ src/Pages/Components/AnimalComponents/AnimalHero.jsx
@@ -1,3 +1,4 @@
+import scroll from '../../../assets/scroll-down.png'
 const AnimalHero = ({ video, animal }) => {
   return (
     <div className="animal-hero">
@@ -5,6 +6,7 @@ const AnimalHero = ({ video, animal }) => {
       <div className="animal-hero-overlay">
         <p>Scroll down to view</p>
         <h1>{animal}</h1>
+        <img src={scroll} alt="scroll down icon" />
       </div>
     </div>
   );
